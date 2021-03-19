3d6562a619ff56b243fc6d5b56636dbc6a6ebea0
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-03-19 15:35:54 -0400

    finished up tasks functionality

--- api/task/router.js
+++ api/task/router.js
@@ -4,4 +4,24 @@ const express = require('express');
 const router = express.Router();
 const Tasks = require('./model');
 
+router.get('/', (req, res) => {
+    Tasks.get()
+    .then(tasks => {
+        res.status(200).json(tasks);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'something went wrong while fetching tasks' });
+    })
+});
+
+router.post('/', (req, res) => {
+    Tasks.insert(req.body)
+    .then(newTask => {
+        res.status(200).json(newTask);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'something went wrong while adding this task' });
+    })
+});
+
 module.exports = router;
